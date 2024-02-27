36329998051561e2969ac65e844dbfb2e42fb855
Charles-Muia <Charles.K.Muia@gmail.com>
2024-02-27 14:28:44 +0300

    Image_Slider

--- index.php
+++ index.php
@@ -14,10 +14,31 @@
 <!-- *********************************************STYLE*************************************************** -->
     <link rel="stylesheet" href="style.css">
 
+<!-- *******************************************GOOGLEFONTS*********************************************** -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&family=Varela+Round&display=swap" rel="stylesheet">
 </head>
 <body>
 
-    
+<!-- *********************************************HEADER************************************************** -->
+      <header>
+        <div class="logo"><img src="Pics/Icon.jpg" alt=""></div>
+            <ul class="menu">
+                <li>home</li>
+                <li>blog</li>
+                <li>info</li>
+            </ul>
+                <div class="search">
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+  </svg>
+                </div>
+      </header>
+
 </body>
 </html>
