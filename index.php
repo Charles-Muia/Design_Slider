141ec6a0b5a43fb91f9e7cf98f423f0c4387c165
Charles-Muia <Charles.K.Muia@gmail.com>
2024-02-27 13:54:50 +0300

    Image_Slider

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- <?php
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Slider Design</title>
+
+<!-- *******************************************FAVICON*************************************************** -->
+  <link rel="icon" href="Pics/Icon.jpg">
+
+<!-- *********************************************STYLE*************************************************** -->
+    <link rel="stylesheet" href="style.css">
+
+
+</head>
+<body>
+
+    
+</body>
+</html>
